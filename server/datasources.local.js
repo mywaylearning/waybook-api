1a630a6a24bc9154ff32e1d4f8b62e5443b581e3
claylo <clay@loveless.net>
2015-05-15 22:47:09 -0700

    support AWS dev env and localdev

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,6 +6,7 @@ module.exports = {
   db: {
     host: process.env.WAYBOOK_DB_HOST,
     user: process.env.WAYBOOK_DB_USER,
-    password: process.env.WAYBOOK_DB_PASSWORD
+    password: process.env.WAYBOOK_DB_PASSWORD,
+    ssl: (process.env.WAYBOOK_DB_SSL !== 'false' || undefined)
   }
 };
