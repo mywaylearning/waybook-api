fec6b65254cfb7f02429a83c397419355a0824ee
claylo <clay@loveless.net>
2015-07-02 23:14:26 -0700

    removing verbose logging, path fix on nginx localdev

--- server/server.js
+++ server/server.js
@@ -1,38 +1,11 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var winston = require('winston');
-var cors = require('cors');
-var expressWinston = require('express-winston');
 
 var app = module.exports = loopback();
-//app.use(cors());
-
-app.use(expressWinston.logger({
-  transports: [
-    new winston.transports.Console({
-      json: true,
-      colorize: true
-    })
-  ],
-  meta: true,
-  msg: 'HTTP {{req.method}} {{req.url}}',
-  expressFormat: true,
-  colorStatus: true
-}));
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
-
-
-app.use(expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console({
-      json: true,
-      colorize: true
-    })
-  ]
-}));
 
 app.start = function() {
   // start the web server
