566fddabf4974456236436acc3ebb8a35f73b5af
lesterzone <lesterzone@gmail.com>
2015-07-30 14:54:35 -0600

    Allow POST /users, access token expiration changed
    Prepare access to create an account. Allow POST to /users.
    Access token expiration changed

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,25 +1,13 @@
 var oauth2 = require('loopback-component-oauth2');
-var debug = require('debug')('waybook:boot:enableAuthentication');
 
 module.exports = function enableAuthentication(server) {
-
-  // enable authentication RBAC system
-  // see loopback/lib/application.js
-  //server.enableAuth();
-
-  // userModel explicitly set here,
-  // in case you cooked up your own versions of those which don't
-  // inherit from LoopBack built-ins.
-
-  // var WaybookUser = server.models.WaybookUser;
-  // var OAuthClientApplication = server.models.OAuthClientApplication;
 
   var options = {
     resourceServer: true,
     dataSource: server.dataSources.db,
     userModel: server.dataSources.db.getModel('WaybookUser', true),
     applicationModel: server.dataSources.db.getModel(
-        'OAuth2ClientApplication', true),
+      'OAuth2ClientApplication', true),
 
     authorizationServer: true,
     authorizePath: '/oauth/authorize',
@@ -37,33 +25,33 @@ module.exports = function enableAuthentication(server) {
     ],
 
     getTTL: function(grantType, clientId, resourceOwner, scopes) {
-      return 9000;
+      /**
+       * Time in seconds to expire access token
+       */
+      return 24000;
     }
   };
 
   oauth2(server, options);
 
-  var auth = oauth2.authenticate({ session: false, scope: 'full' });
+  var auth = oauth2.authenticate({
+    session: false,
+    scope: 'full'
+  });
+
   server.middleware('auth:before', [
     '/me',
     '/Posts',
     '/Goals',
-    '/Users',
+    // '/Users', POST to /users should be public
     '/User'
   ], auth);
 
-  server.get('/me', function(req, res) {
-    //console.dir(req);
-    res.json({ 'user_id': req.user.id, name: req.user.username,
-      accessToken: req.authInfo.accessToken });
+  server.get('/me', function(request, response) {
+    response.json({
+      'user_id': request.user.id,
+      name: request.user.username,
+      accessToken: request.authInfo.accessToken
+    });
   });
-  //
-  // var setCurrentUser = function(req, res, next) {
-  //   // how do we check
-  //   server.models.WaybookUser.getAuthenticatedUser(req, function(err, user) {
-  //
-  //   });
-  // };
-  // server.use(setCurrentUser);
-
 };
