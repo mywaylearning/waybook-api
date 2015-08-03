8a467f9e907bb4d70686dfab77cbdcd2f376c9fc
lesterzone <lesterzone@gmail.com>
2015-08-03 15:37:59 -0600

    GET Tags definition
    Defined tags endpont to filter tags based on search param

--- common/models/tag.js
+++ common/models/tag.js
@@ -12,4 +12,22 @@ module.exports = function(Tag) {
 
     return Tag.findOrCreate(where, callback);
   };
+
+  /**
+   * Get tags based on input filter
+   */
+  Tag.tagsIndex = function(input, request, callback) {
+
+    return Tag.find({
+      where: {
+        text: {
+          like: '%' + input + '%'
+        }
+      }
+    }, function   (error, data) {
+      console.log(error || data)
+      return callback(null, data);
+    });
+  };
+
 };
