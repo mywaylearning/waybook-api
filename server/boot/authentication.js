8a61ee3bb99e78e0c81cdbb87c6b126f2e4925d6
lesterzone <lesterzone@gmail.com>
2015-08-05 16:10:47 -0600

    Disable public access to create contact
    User must be logged to create a contact

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -44,6 +44,7 @@ module.exports = function enableAuthentication(server) {
     '/Posts',
     '/Goals',
     '/Tags',
+    '/contacts',
     '/comments',
     // '/Users', POST to /users should be public
     '/User'
