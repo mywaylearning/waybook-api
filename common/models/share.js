462fe14ddee8e2eaa6e5c7da43eda577397a526c
lesterzone <lesterzone@gmail.com>
2015-08-28 10:54:35 -0600

    Allow Share to include `shareWith` from new user
    Once user creates an account, Contact table will be updated with
    `waybookId` property associated to new created account. After that,
    Share table will be udated with `shareWith` property.
    This allows new account to see shared posts with

--- common/models/share.js
+++ common/models/share.js
@@ -5,11 +5,36 @@ var shareParallel = require('../helpers/shareParallel');
 
 module.exports = function(Share) {
 
-    Share.withMany = function(data, callback){
+    Share.withMany = function(data, callback) {
         var parallel = shareParallel(data, Share);
 
-        async.parallel(parallel, callback || function(error, results){
+        async.parallel(parallel, callback || function(error, results) {
             console.log('after save all share', error, results);
+        });
+    };
+
+    Share.updateShareWith = function(ids, userId, callback) {
+        console.log('ids', ids);
+        return Share.find({
+            where: {
+                withContact: {
+                    inq: ids
+                }
+            }
+        }, function(error, shares) {
+            if (error) {
+                return callback(error, null);
+            }
+
+            var ids =  shares.map(function(share) {
+                share.sharedWith = userId;
+                share.withContact = null;
+
+                console.log('about to upsert share', share.id);
+                Share.upsert(share);
+                return share.id;
+            });
+            return callback(null, ids);
         });
     };
 };
