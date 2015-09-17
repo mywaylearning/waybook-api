62ef1a5a4d15f8d93698c989cecaeeee5163331c
lesterzone <lesterzone@gmail.com>
2015-09-17 11:29:22 -0600

    Unit test (integration) to load toml file
    Using base.toml file, parse and test lib helper

---
+++ tests/libs/loadToml.test.js
@@ -0,0 +1,13 @@
+'use strict';
+var tape = require('tape');
+var loadToml = require('../../lib/loadToml');
+
+tape('Load Toml files', function(test) {
+    test.plan(1);
+
+    var callback = function(error, data) {
+        test.equal(data.meta.name, 'base', 'proper name from parse base.toml file');
+    };
+
+    loadToml('./explorations/base.toml', callback);
+});
