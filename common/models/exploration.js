97d8f1ab974b0c92b7b1a8e0bfbe907b99febe55
lesterzone <lesterzone@gmail.com>
2015-09-25 10:29:34 -0600

    Get results for asq exploration
    Based on exploration id and results param, return proper score object

--- common/models/exploration.js
+++ common/models/exploration.js
@@ -1,6 +1,7 @@
 'use strict';
 var reject = require('../helpers/reject');
 var fromArray = require('../helpers/fromArray');
+var asq = require('../../lib/asq');
 
 module.exports = function(Exploration) {
 
@@ -54,11 +55,54 @@ module.exports = function(Exploration) {
         return Exploration.all(query, callback);
     };
 
+    function getResults(request, callback) {
+        if (!request.user || !request.user.id) {
+            return reject('user required', callback);
+        }
+
+        var query = {
+            where: {
+                explorationId: request.query.explorationId,
+                userId: request.user.id
+            }
+        };
+
+        Exploration.app.models.Record.find(query, function(error, records) {
+            var responses = {};
+            records.map(function(model) {
+                responses[model.question] = model.answer;
+            });
+
+            var agree = [
+                2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21,
+                22, 23, 26, 33, 35, 39, 41,
+                42, 43, 45, 46
+            ];
+
+            var disagree = [
+                1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28,
+                29, 30, 31, 32, 34, 36, 37,
+                38, 40, 44, 47, 48, 49, 50
+            ];
+
+            asq(agree, disagree, responses, function(score) {
+                return callback(null, {
+                    score: score
+                });
+            });
+        });
+    }
+
     Exploration.getExploration = function(slug, request, callback) {
+        if (request.query.results, request.query.explorationId) {
+            return getResults(request, callback);
+        }
+
         var user = request.user;
 
         var fields = [
-            'name', 'slug', 'pattern', 'image', 'id', 'description', 'category'
+            'name', 'slug', 'pattern', 'image', 'id',
+            'description', 'category'
         ];
 
         var query = {
