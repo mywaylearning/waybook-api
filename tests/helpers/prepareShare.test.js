b29a0512ec9d929537a491dd2bdc19b4fc0f4d33
lesterzone <lesterzone@gmail.com>
2015-09-25 10:27:45 -0600

    Better test output for prepare share tests
    Use better label on output

--- tests/helpers/prepareShare.test.js
+++ tests/helpers/prepareShare.test.js
@@ -21,10 +21,25 @@ tape('prepareShare helper', function(test) {
 
     var emailData = prepareShare(post, user);
 
-    test.equal(emailData.to[0], 'foo@bar.com');
-    test.equal(emailData.substitutions['-firstName-'][0], 'foo');
-    test.equal(emailData.substitutions['-learnerFirstName-'][0], 'bob');
-    test.equal(emailData.substitutions['-learnerLastName-'][0], 'marley');
-    test.equal(emailData.substitutions['-postType-'][0], 'goal');
-    // test.equal(emailData.substitutions['-link-'][0], '');
+    test.equal(emailData.to[0], 'foo@bar.com', 'should return proper email field');
+
+    test.equal(
+        emailData.substitutions['-firstName-'][0], 'foo',
+        'should return proper names'
+    );
+
+    test.equal(
+        emailData.substitutions['-learnerFirstName-'][0], 'bob',
+        'should return proper learner fist name substitutions'
+    );
+
+    test.equal(
+        emailData.substitutions['-learnerLastName-'][0], 'marley',
+        'should return proper last names substitutions'
+    );
+
+    test.equal(
+        emailData.substitutions['-postType-'][0], 'goal',
+        'should return proper post types substitutions'
+    );
 });
