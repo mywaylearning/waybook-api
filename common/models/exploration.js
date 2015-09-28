425e45f23dec6242e607cffd186cdb9511e38b84
lesterzone <lesterzone@gmail.com>
2015-09-28 12:02:06 -0600

    Based on algorithm compute results for exploration
    Using defined algorithm name from toml file, compute result for an
    specific exploration

--- common/models/exploration.js
+++ common/models/exploration.js
@@ -2,6 +2,23 @@
 var reject = require('../helpers/reject');
 var fromArray = require('../helpers/fromArray');
 var asq = require('../../algorithms/asq');
+var big5 = require('../../algorithms/big5');
+
+/**
+ * Hardcoded values for asq algorithm, will be added to the toml file in the next
+ * relesease of way product
+ */
+var agree = [
+    2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21,
+    22, 23, 26, 33, 35, 39, 41,
+    42, 43, 45, 46
+];
+
+var disagree = [
+    1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28,
+    29, 30, 31, 32, 34, 36, 37,
+    38, 40, 44, 47, 48, 49, 50
+];
 
 module.exports = function(Exploration) {
 
@@ -62,35 +79,48 @@ module.exports = function(Exploration) {
 
         var query = {
             where: {
-                explorationId: request.query.explorationId,
-                userId: request.user.id
-            }
+                id: request.query.explorationId,
+            },
+            include: ['records']
         };
 
-        Exploration.app.models.Record.find(query, function(error, records) {
+        Exploration.findOne(query, function(error, exploration) {
+            if (error) {
+                return callback(error);
+            }
+
+            if (!exploration) {
+                return reject('not found', callback);
+            }
+
+            if(!exploration.algorithm){
+                return reject('algorithm required', callback);
+            }
+
+            exploration = exploration.toJSON();
             var responses = {};
 
-            records.map(function(model) {
+            exploration.records.map(function(model) {
                 responses[model.question] = model.answer;
             });
 
-            var agree = [
-                2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21,
-                22, 23, 26, 33, 35, 39, 41,
-                42, 43, 45, 46
-            ];
 
-            var disagree = [
-                1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28,
-                29, 30, 31, 32, 34, 36, 37,
-                38, 40, 44, 47, 48, 49, 50
-            ];
-
-            asq(agree, disagree, responses, function(score) {
-                return callback(null, {
-                    score: score
+            /**
+             * TODO: Use algorithms object to define proper function to be used
+             */
+            if (exploration.algorithm === 'asq') {
+                return asq(agree, disagree, responses, function(score) {
+                    return callback(null, {
+                        score: score
+                    });
                 });
-            });
+            }
+
+            if (exploration.algorithm === 'big5') {
+                return big5(responses, function(data) {
+                    return callback(null, data);
+                });
+            }
         });
     }
 
