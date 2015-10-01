4f6012629dde5f76d7cba2a12e58254b1bcccf8a
lesterzone <lesterzone@gmail.com>
2015-10-01 08:42:50 -0600

    Add result fields to Exploration table
    resultExplanation
    resultDisplayType

---
+++ migrations/sqls/20151001143129-addExplanationToExploration-down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `Exploration` DROP `resultExplanation`;
+ALTER TABLE `Exploration` DROP `resultExplanation`;
+ALTER TABLE `Exploration` DROP `resultDisplayType`;
---
+++ migrations/sqls/20151001143129-addExplanationToExploration-up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `Exploration` ADD COLUMN `resultExplanation` TEXT;
+ALTER TABLE `Exploration` ADD COLUMN `resultDisplayType` varchar(128);
