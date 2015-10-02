c524ad904d8523695a8e70713d307301094d58b3
lesterzone <lesterzone@gmail.com>
2015-10-02 09:44:51 -0600

    Request access token expiration
    Changed to longer time

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -4,60 +4,60 @@ var oauth2 = require('loopback-component-oauth2');
 
 module.exports = function enableAuthentication(server) {
 
-  var options = {
-    resourceServer: true,
-    dataSource: server.dataSources.db,
-    userModel: server.dataSources.db.getModel('WaybookUser', true),
-    applicationModel: server.dataSources.db.getModel(
-      'OAuth2ClientApplication', true),
+    var options = {
+        resourceServer: true,
+        dataSource: server.dataSources.db,
+        userModel: server.dataSources.db.getModel('WaybookUser', true),
+        applicationModel: server.dataSources.db.getModel(
+            'OAuth2ClientApplication', true),
 
-    authorizationServer: true,
-    authorizePath: '/oauth/authorize',
-    tokenPath: '/oauth/token',
-    decisionPath: '/oauth/authorize/decision',
+        authorizationServer: true,
+        authorizePath: '/oauth/authorize',
+        tokenPath: '/oauth/token',
+        decisionPath: '/oauth/authorize/decision',
 
-    loginPage: '/login',
-    loginPath: false,
+        loginPage: '/login',
+        loginPath: false,
 
-    supportedGrantTypes: [
-      'authorizationCode',
-      'refreshToken',
-      'implicit',
-      'resourceOwnerPasswordCredentials'
-    ],
+        supportedGrantTypes: [
+            'authorizationCode',
+            'refreshToken',
+            'implicit',
+            'resourceOwnerPasswordCredentials'
+        ],
 
-    getTTL: function(grantType, clientId, resourceOwner, scopes) {
-      /**
-       * Time in seconds to expire access token
-       */
-      return 24000;
-    }
-  };
+        getTTL: function( /*grantType, clientId, resourceOwner, scopes*/ ) {
+            /**
+             * Time in seconds to expire access token
+             */
+            return 172800;
+        }
+    };
 
-  oauth2(server, options);
+    oauth2(server, options);
 
-  var auth = oauth2.authenticate({
-    session: false,
-    scope: 'full'
-  });
-
-  server.middleware('auth:before', [
-    '/me',
-    '/Posts',
-    '/Goals',
-    '/Tags',
-    '/contacts',
-    '/explorations',
-    '/comments',
-    // '/Users', POST to /users should be public
-    '/User'
-  ], auth);
-
-  server.get('/me', function(request, response) {
-    response.json({
-      'user_id': request.user.id,
-      name: request.user.username,
-      accessToken: request.authInfo.accessToken
+    var auth = oauth2.authenticate({
+        session: false,
+        scope: 'full'
     });
-  });
+
+    server.middleware('auth:before', [
+        '/me',
+        '/Posts',
+        '/Goals',
+        '/Tags',
+        '/contacts',
+        '/explorations',
+        '/comments',
+        // '/Users', POST to /users should be public
+        '/User'
+    ], auth);
+
+    server.get('/me', function(request, response) {
+        response.json({
+            'user_id': request.user.id,
+            name: request.user.username,
+            accessToken: request.authInfo.accessToken
+        });
+    });
 };
