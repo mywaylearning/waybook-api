f7840dfc2688a35dab04875b21a0a235952e9ee6
lesterzone <lesterzone@gmail.com>
2015-12-07 09:34:22 -0600

    Add owner to query contacts and posts if present
    Query to database with owner if present on query string

--- common/lib/search.js
+++ common/lib/search.js
@@ -1,3 +1,7 @@
+/**
+ * Search posts and contacts by tag, owner and type
+ */
+
 'use strict';
 
 var includes = require('lodash.includes');
@@ -8,17 +12,26 @@ var fromArray = require('../helpers/fromArray');
 /**
  * Search Contacts or Posts by tag associated to current user
  */
-module.exports = function(tag, request, callback) {
+module.exports = function(tag, ownerId, request, callback) {
     var Post = this.app.models.Post;
     var Contact = this.app.models.Contact;
+
     var query = {
         where: {
             userId: request.user.id
         }
     };
 
+    /**
+     * Include owner in query filter
+     */
+    if (ownerId) {
+        query.where.userId = ownerId;
+    }
+
     return async.parallel({
         contacts: function(after) {
+
             Contact.find(query, function(error, contacts) {
                 if (error) {
                     return callback(error, null);
