660661a4f0dd013bdca1bb0c6cf455af4fc319ec
lesterzone <lesterzone@gmail.com>
2016-02-10 17:08:10 -0600

    Lib helper to create discovery post from explora
    From exploration results, create a discovery Post

---
+++ common/lib/shareResults.js
@@ -0,0 +1,33 @@
+/**
+ * Based on explorations results, create a discovery post
+ */
+'use strict';
+
+const DISCOVERY = 'discovery';
+
+module.exports = options => {
+
+    let data = options.data;
+    let user = options.user;
+    let Post = options.model.app.models.Post;
+    let callback = options.callback;
+
+    let post = {};
+
+    post.results = Object.keys(data).map(key => {
+        let property = {};
+        property[key] = data[key];
+        return property;
+    });
+
+    post.userId = user.id;
+    post.postType = DISCOVERY;
+
+    return Post.create(post, function(error, created) {
+        if (error) {
+            return callback(error);
+        }
+
+        return callback(null, created);
+    });
+};
