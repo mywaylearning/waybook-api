37426cd18e161f4c7c863208802b8631fd8d6629
lesterzone <lesterzone@gmail.com>
2016-02-19 10:08:19 -0600

    Include shared resources 10 years plan
    Include shared resources on 10 years plan response

--- common/lib/reporting.js
+++ common/lib/reporting.js
@@ -7,10 +7,36 @@ const async = require('async');
 const reject = require('../helpers/reject');
 const classifyPosts = require('../helpers/classifyPosts');
 
+function mapShared(models) {
+    let result = [];
+    models.map(model => {
+        let object = model.toJSON();
+
+        if (!object.Post || !object.WaybookUser) {
+            return;
+        }
+
+        /**
+         * As per requirements, only resource posts should be included.
+         * This can be achieved by using proper query, but current version
+         * of loopback doesn't allow double `where` query. One on share and
+         * one on Post model association.
+         */
+        if (object.Post.postType !== 'resource') {
+            return;
+        }
+
+        object.Post.WaybookUser = object.WaybookUser;
+        result.push(object.Post);
+    });
+    return result;
+}
+
 module.exports = (request, User, callback) => {
     let currentUser = request.user;
     let Post = User.app.models.Post;
     let Contact = User.app.models.Contact;
+    let Share = User.app.models.Share;
     let parallel = {};
 
     let postsQuery = {
@@ -23,6 +49,20 @@ module.exports = (request, User, callback) => {
         where: {
             userId: currentUser.id
         }
+    };
+
+    let sharedQuery = {
+        where: {
+            sharedWith: currentUser.id
+        },
+        include: [{
+            relation: 'WaybookUser',
+            scope: {
+                fields: ['id', 'email', 'firstName', 'lastName', 'username']
+            }
+        }, {
+            relation: 'Post'
+        }]
     };
 
     /**
@@ -39,6 +79,10 @@ module.exports = (request, User, callback) => {
         return Contact.find(contactsQuery, after);
     };
 
+    parallel.shared = (after) => {
+        return Share.find(sharedQuery, after);
+    };
+
     let asyncCallback = (error, data) => {
         if (error) {
             console.log('error on load posts', error);
@@ -47,7 +91,7 @@ module.exports = (request, User, callback) => {
 
         let result = classifyPosts(data.posts);
         result.contacts = data.contacts || {};
-
+        result.shared = mapShared(data.shared || []);
         return callback(null, result);
     };
 
